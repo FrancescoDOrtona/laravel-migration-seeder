3194a18a864e75b27f20a622a2ceff145a2989ab
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-13 16:09:28 +0100

    Add: route, controller function & seeder

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $today = Carbon::now()->toDateString();
+        $trains = Train::whereDate('orario_partenza', $today)
+            ->where('in_orario', true)
+            ->orderBy('orario_partenza')
+            ->get();
+
+        return view('home', compact('trains'));
+    }
+
 }
