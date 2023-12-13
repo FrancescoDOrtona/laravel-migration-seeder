4dd1182938fc5494d178063a96ef83a055ab9763
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-13 16:24:14 +0100

    Add: train seed example content

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,18 @@ class TrainSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Train::create([
+            'azienda' => 'Trenitalia',
+            'stazione_partenza' => 'Roma',
+            'stazione_arrivo' => 'Milano',
+            'orario_partenza' => '08:00:00',
+            'orario_arrivo' => '17:00:00',
+            'codice_treno' => '123ABC',
+            'numero_carrozze' => 3,
+            'in_orario' => true,
+            'cancellato' => false,
+        ]);
     }
 }
