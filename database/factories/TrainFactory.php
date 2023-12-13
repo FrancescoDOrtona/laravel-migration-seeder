0a9b568e347ddddf052da80ac1315b5b77e5477d
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-13 16:29:39 +0100

    Add: Train Factory + changed train seed to insert data in DB

---
+++ database/factories/TrainFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Train;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Train>
+ */
+class TrainFactory extends Factory
+{
+    protected $model = Train::class;
+    public function definition()
+    {
+        return [
+            'azienda' => $this->faker->company,
+            'stazione_partenza' => $this->faker->city,
+            'stazione_arrivo' => $this->faker->city,
+            'orario_partenza' => $this->faker->time,
+            'orario_arrivo' => $this->faker->time,
+            'codice_treno' => $this->faker->word,
+            'numero_carrozze' => $this->faker->numberBetween(1, 10),
+            'in_orario' => $this->faker->boolean,
+            'cancellato' => $this->faker->boolean,            
+        ];
+    }
+}
--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -13,16 +13,6 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        Train::create([
-            'azienda' => 'Trenitalia',
-            'stazione_partenza' => 'Roma',
-            'stazione_arrivo' => 'Milano',
-            'orario_partenza' => '08:00:00',
-            'orario_arrivo' => '17:00:00',
-            'codice_treno' => '123ABC',
-            'numero_carrozze' => 3,
-            'in_orario' => true,
-            'cancellato' => false,
-        ]);
+        Train::factory(14)->create();
     }
 }
